3323c2fde73d453079d00caa3091dde31a2b5ce3
Serendey_° <80664816+B-KJS26@users.noreply.github.com>
2022-05-19 21:32:43 +0900

    Add files via upload

---
+++ ApiMeal/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // ignore: unused_field
+  int _counter = 0;
+
+  // ignore: unused_element
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext  context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: const Color(0xff9EC3FF),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+            onPressed: () {
+              // ignore: avoid_print
+              print('menu button is clicked');
+            },
+          ),
+        ),
+      body: Center(
+        child: Column(
+          children: const <Widget>[
+            Text(
+              '식단표',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 30,
+                fontFamily: "wow"
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: const Color(0xff9EC3FF),
+    );
+  }
+}
