48234d8248c7c9cc12077974ff03bf2741320e13
Serendey_° <80664816+B-KJS26@users.noreply.github.com>
2022-05-24 21:16:59 +0900

    Update main.dart

--- ApiMeal/main.dart
+++ ApiMeal/main.dart
@@ -1,80 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() =>  runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'sikdan',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: ''),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const MyHomePage({ Key? key }) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // ignore: unused_field
-  int _counter = 0;
-
-  // ignore: unused_element
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
-  Widget build(BuildContext  context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: const Color(0xff9EC3FF),
-        centerTitle: true,
-        elevation: 0.0,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-            onPressed: () {
-              // ignore: avoid_print
-              print('menu button is clicked');
-            },
-          ),
-        ),
-      body: Center(
-        child: Column(
-          children: const <Widget>[
-            Text(
-              '식단표',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 30,
-              ),
-            ),
-            Text(
-              '5월 23일(월)',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 26,
-              ),
-            ),
-          ],
-        ),
-      ),
       backgroundColor: const Color(0xff9EC3FF),
+      // ignore: avoid_unnecessary_containers
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(icon: const Icon(Icons.menu), onPressed: () { 
+            print("누름");
+          },
+          color: Colors.white,
+        ),],
+      ),
     );
   }
 }
