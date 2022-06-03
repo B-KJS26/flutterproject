75119f77be7782c6c0d67dadd3054ea2711a346e
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-06-03 23:17:40 +0900

    Update main.dart

--- ApiMeal/main.dart
+++ ApiMeal/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,48 +23,63 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff9EC3FF),
-      // ignore: avoid_unnecessary_containers
       body: Row(
         children: [
-          IconButton(
-            icon: const Icon(Icons.menu), 
-              iconSize: 30,
+          // ignore: sized_box_for_whitespace
+          Container(
+            width: 30 * (MediaQuery.of(context).size.width/360),
+            child: Column(
+              children: [
+              IconButton(
+                icon: const Icon(Icons.menu), 
+                iconSize: 30,
                 onPressed: () {
                  // ignore: avoid_print
                   print("누름");
                 },
                   color: Colors.white,
+              ),
+            ]),
           ),
-
-          Container(width: ScreenUtil().setWidth(300),),
-          //아침, 점심, 저녁
-          Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+          
+          SizedBox(
+            width: 260 * (MediaQuery.of(context).size.width/360),
+          ),
+          Container(
+            width: 70* (MediaQuery.of(context).size.width/360),
+            height: 350 * (MediaQuery.of(context).size.height/360),
+            child:Column(
               children:[
-          Column(
-            children:[   
-              IconButton(
+                IconButton(
                   icon: Image.asset('assets/images/sun.png'),
                   iconSize: 40,
-                  onPressed: () {},
-              ),
+                  onPressed: () {
+                    print('아침');
+                  },
+                ),
 
-              IconButton(
+                IconButton(
                   icon: Image.asset('assets/images/noon.png'),
                   iconSize: 40,
-                  onPressed: () {},
-              ),
+                  onPressed: () {
+                    print('점심');
+                  },
+                ),
 
-              IconButton(
+                IconButton(
                   icon: Image.asset('assets/images/moon.png'),
                   iconSize: 40,
-                  onPressed: () {},
+                  onPressed: () {
+                    print('저녁');
+                  },
               ),
           ]),
-        ])
-    ],));
+          ),
+        ],)
+    );
   }
 }
