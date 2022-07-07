8aecb8de54204be5c7aa782583824484a879e1f9
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-07-07 22:33:35 +0900

    Add files via upload

---
+++ ApiMeal/navi.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+// ignore: camel_case_types
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Navi'),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Text("home"),
+              ),
+              Center(
+                child: Text("music"),
+              ),
+              Center(
+                child: Text("apps"),
+              ),
+              Center(
+                child: Text("settings"),
+              ),
+            ],
+          ),
+          extendBodyBehindAppBar: true, // add this line
+
+          bottomNavigationBar: Container(
+            color: Colors.white, //색상
+            child: Container(
+              height: 70,
+              padding: const EdgeInsets.only(bottom: 10, top: 5),
+              child: const TabBar(
+                //tab 하단 indicator size -> .label = label의 길이
+                //tab 하단 indicator size -> .tab = tab의 길이
+                indicatorSize: TabBarIndicatorSize.label,
+                //tab 하단 indicator color
+                indicatorColor: Colors.red,
+                //tab 하단 indicator weight
+                indicatorWeight: 2,
+                //label color
+                labelColor: Colors.red,
+                //unselected label color
+                unselectedLabelColor: Colors.black38,
+                labelStyle: TextStyle(
+                  fontSize: 13,
+                ),
+                tabs: [
+                  Tab(
+                    icon: Icon(
+                      Icons.home_outlined,
+                    ),
+                    text: 'Home',
+                  ),
+                  Tab(
+                    icon: Icon(Icons.music_note),
+                    text: 'Music',
+                  ),
+                  Tab(
+                    icon: Icon(
+                      Icons.apps,
+                    ),
+                    text: 'Apps',
+                  ),
+                  Tab(
+                    icon: Icon(
+                      Icons.settings,
+                    ),
+                    text: 'Settings',
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
