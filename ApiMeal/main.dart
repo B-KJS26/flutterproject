70d950c1d92218d9ff55258ac942c1ea4867f06e
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-07-10 21:09:09 +0900

    Update main.dart

--- ApiMeal/main.dart
+++ ApiMeal/main.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+// ignore_for_file: non_constant_identifier_names
 
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:http/http.dart';
+// ignore: unused_import
+import 'package:meal/post.dart';
+// ignore: unused_import
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import 'dart:ui';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -23,7 +31,15 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+  // ignore: unnecessary_new
+  var now = new DateTime.now();
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  // ignore: override_on_non_overriding_member
+  final standardDeviceWidth = 360;
+  String Month = DateFormat('M').format(now);
+  String Day = DateFormat('d').format(now);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,99 +52,88 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: [
                     // ignore: sized_box_for_whitespace
                     Container(
-                      margin: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-                      width: 70,
-                      height: 70,
+                      margin: const EdgeInsets.fromLTRB(30, 30, 0, 0),
+                      width: 70 * ((MediaQuery.of(context).size.width) /    standardDeviceWidth),
+                      height: 35 * ((MediaQuery.of(context).size.height) / 360),
                       child: Image.asset('assets/images/spoonandfork.png'),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,//테두리설정
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                        // ignore: prefer_const_literals_to_create_immutables
+                      decoration: BoxDecoration(
+                        color: Colors.white,//테두리
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.white70, width: 1),
                         boxShadow: [
                           // ignore: prefer_const_constructors
                           BoxShadow(
-                            color: Colors.grey,
-                            offset: const Offset(4.0, 4.0),
-                            blurRadius: 15.0,
+                            color: Colors.grey.shade900,
+                            offset: const Offset(0, 4.0),
+                            blurRadius: 10.0,
                             spreadRadius: 1.0,
                           ),
-                          BoxShadow(
+                          const BoxShadow(
                             color: Colors.white,
-                            offset: Offset(-4.0, -4.0),
-                            blurRadius: 15.0,
+                            offset: Offset(-1.0, -1.0),
+                            blurRadius: 10.0,
                             spreadRadius: 1.0,
                           ),
                         ]
                     ),),
 
                     Container(
-                    margin: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-                    child: const Text(
-                    '급식',
-                    style: TextStyle(
+                    margin: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                    child: Text(
+                      '급식 \n' 
+                      '$Month월 '
+                      '$Day일',
+                    style: const TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
-              ),),),
+                    ),),),
               ],),),
-          //시간표가 들어갈 컨테이너입니다.
-          const SizedBox(
-            height: 30,
-          ),
-
+              const SizedBox(
+                height: 20,
+              ),
           Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: MediaQuery.of(context).size.height * 0.67,
-                decoration: BoxDecoration(
+            width: 300 * ((MediaQuery.of(context).size.width) /    standardDeviceWidth),
+            height: 260 * ((MediaQuery.of(context).size.height) / 360),
+            decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/Calender.png'),
+                  ),
                   borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
                   boxShadow: [
-                          // ignore: prefer_const_constructors
-                          BoxShadow(
-                            color: Colors.grey,
-                            offset: const Offset(4.0, 4.0),
-                            blurRadius: 15.0,
+                    BoxShadow(
+                      color: Colors.grey.shade400,
+                            offset: const Offset(0, 12.0),
+                            blurRadius: 10.0,
                             spreadRadius: 1.0,
-                          ),
-                          BoxShadow(
-                            color: Colors.white,
-                            offset: Offset(-4.0, -4.0),
-                            blurRadius: 15.0,
-                            spreadRadius: 1.0,
-                          ),
-                        ]
-            ),
+                    ),
+                  ],
+            ),   
           ),
-          
         ],
       ),
-      //바텀내비게이션 바
-    bottomNavigationBar: BottomNavigationBar(
-      items: const<BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.text_snippet),
-          label: '홈',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.text_snippet),
-          label: '메뉴',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.text_snippet),
-          label: 'wㅎㅎ',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.text_snippet),
-          label: 'ㄴㄴㄴ',
-        ),
-        BottomNavigationBarItem(
+      bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: '홈',
+            label: 'Tlqkf',
           ),
-      ],
-    ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Nomonm',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Nomonm',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Nomonm',
+          ),
+        ],
+      ),
     );
   }
 }
