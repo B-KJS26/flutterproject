ea2db17edece1317e17147adbe5a573603e16ede
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-08-17 21:31:28 +0900

    Update main.dart

--- ApiMeal/main.dart
+++ ApiMeal/main.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: non_constant_identifier_names
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:http/http.dart';
+// ignore: unused_import
 import 'package:meal/post.dart';
+// ignore: unused_import
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'dart:ui';
@@ -31,13 +35,14 @@ class MyHomePage extends StatefulWidget {
   var now = new DateTime.now();
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  // ignore: override_on_non_overriding_member
   final standardDeviceWidth = 360;
   String Month = DateFormat('M').format(now);
   String Day = DateFormat('d').format(now);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color(0xffffffff),
       body: Column(
         children: [
           //FLEXIBLE을 이용하여 화면이 어떻게 달라져도 자동 조절
@@ -46,21 +51,20 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: [
                     // ignore: sized_box_for_whitespace
                     Container(
-                      margin: const EdgeInsets.fromLTRB(30, 30, 0, 0),
-                      width: 70 * ((MediaQuery.of(context).size.width) /    standardDeviceWidth),
-                      height: 35 * ((MediaQuery.of(context).size.height) / 360),
+                      margin: const EdgeInsets.fromLTRB(20, 45, 0, 0),
+                      height: MediaQuery.of(context).size.height * 0.1,
                       child: Image.asset('assets/images/spoonandfork.png'),
                       decoration: BoxDecoration(
-                        color: Colors.white,//테두리
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.white70, width: 1),
+                        color: const Color(0xffF9F9F9),//테두리
+                        borderRadius: BorderRadius.circular(22),
+                        border: Border.all(color: const Color(0xffF9F9F9), width: 7),
                         boxShadow: [
                           // ignore: prefer_const_constructors
                           BoxShadow(
-                            color: Colors.grey.shade900,
-                            offset: const Offset(0, 4.0),
-                            blurRadius: 10.0,
-                            spreadRadius: 1.0,
+                            color: Colors.grey.shade500,
+                            offset: const Offset(0, 5.0),
+                            blurRadius: 5.0,
+                            spreadRadius: 0.3,
                           ),
                           const BoxShadow(
                             color: Colors.white,
@@ -72,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),),
 
                     Container(
-                    margin: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                    margin: const EdgeInsets.fromLTRB(20, 55, 0, 0),
                     child: Text(
                       '급식 \n' 
                       '$Month월 '
@@ -86,20 +90,20 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 20,
               ),
           Container(
-            width: 300 * ((MediaQuery.of(context).size.width) /    standardDeviceWidth),
-            height: 260 * ((MediaQuery.of(context).size.height) / 360),
+            width:  0.92 * MediaQuery.of(context).size.width,
+            height: 0.71 * MediaQuery.of(context).size.height,
             decoration: BoxDecoration(
                   image: const DecorationImage(
                     fit: BoxFit.fill,
-                    image: AssetImage('assets/images/Calender.png'),
+                    image: AssetImage('assets/images/Cal.png'),
                   ),
                   borderRadius: BorderRadius.circular(10),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.grey.shade400,
-                            offset: const Offset(0, 12.0),
-                            blurRadius: 10.0,
-                            spreadRadius: 1.0,
+                            offset: const Offset(0, 20.0),
+                            blurRadius: 30.0,
+                            spreadRadius: 0,
                     ),
                   ],
             ),   
